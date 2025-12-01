d584333002ce86ee4084166bf33112263f43f1f0
kirillchertok <kirill250305@gmail.com>
2025-12-01 12:44:42 +0300

    add idl

--- client/src/constants/token.ts
+++ client/src/constants/token.ts
@@ -1,6 +1,6 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const REWARD_TOKEN_MINT = new PublicKey('DCV2LkCa623dRcBPFEfcDJFVYtYqUrWPRvX1zVCXVrjS');
+export const REWARD_TOKEN_MINT = new PublicKey('24tVjgWS8UChGwPdP7sNGSs6Qkkfva1PgmfN6SZkzoTs');
 
 // Standard SPL tokens use 9 decimals (like SOL)
 export const TOKEN_DECIMAL = 1_000_000_000;
